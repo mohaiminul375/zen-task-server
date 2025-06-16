86c6b8f8e988f63638e60ed6918b2f749d5409f0
mohaiminul375 <mohaiminul375@gmail.com>
2025-06-16 18:40:24 +0600

    todo updated successfully

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -28,6 +28,22 @@ router.post('/create-todo', async (req, res) => {
         res.status(500).json({ message: `failed to create todo: ${error}` });
     }
 })
+// update a todo
+router.patch('/update-todo/:id', async (req, res) => {
+  
+    try {
+        await Todo.updateOne({ _id: req.params.id }, {
+            $set: {
+                ...req.body
+            }
+        })
+
+        res.status(200).json({ success: true, message: 'todo was updated' });
+    } catch (error) {
+        res.status(500).json({ message: `failed to update todo: ${error}` });
+    }
+
+})
 // delete a todo
 router.delete('/:id', async (req, res) => {
     console.log('hited')
